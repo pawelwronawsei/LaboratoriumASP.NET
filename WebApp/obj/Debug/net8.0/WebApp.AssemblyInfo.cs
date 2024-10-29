abb67d5df1f74e72df30e789c4599f107da57ad3
Rimpy99 <70006pawel@gmail.com>
2024-10-29 18:35:42 +0100

    lab4 commit2

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2975d55f079462d8adf0a8830c98d66df95f9eff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b969309d6e22883c4da2b1dc61ee3e0dff64517")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
